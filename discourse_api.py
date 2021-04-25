248e8b977913f72e5124d7eb801106d84b50d53b
guipenedo <nostrumg@gmail.com>
2021-04-25 17:18:21 +0100

    now replaces the "moderation" group with the provided group mapping

--- discourse_api.py
+++ discourse_api.py
@@ -26,7 +26,9 @@ def get_about_post(category):
             return client.post(comps[2], comps[3])
 
 
-def duplicate_category(old, name, slug, parent=None, sub_slug="", group_mapping={}):
+def duplicate_category(old, name, slug, parent=None, sub_slug="", group_mapping=None):
+    if group_mapping is None:
+        group_mapping = {}
     old = client._get(u"/c/{0}{1}/find_by_slug.json".format(sub_slug, old["slug"]))["category"]  # more info
     data = {}
     for x in old:
@@ -35,12 +37,19 @@ def duplicate_category(old, name, slug, parent=None, sub_slug="", group_mapping=
             if data[x] == False:
                 data[x] = 'false'
     # description_excerpt, description, uploaded_logo, description_text, navigate_to_first_post_after_read, permission
+    # use the new slug
     data["slug"] = slug
+    # images
     if "uploaded_logo" in old and old["uploaded_logo"]:
         data["uploaded_logo_id"] = old["uploaded_logo"]["id"]
     if "uploaded_background" in old and old["uploaded_background"]:
         data["uploaded_background_id"] = old["uploaded_background"]["id"]
+    # permissions are cleared initially
     data["permissions"] = {}
+    # reviewer group also has to be replaced
+    if 'reviewable_by_group_name' in old and old['reviewable_by_group_name'] in group_mapping:
+        data['reviewable_by_group_name'] = group_mapping[old['reviewable_by_group_name']]
+    # all access permissions, possibly with group mapping
     if 'group_permissions' in old:
         for g in old['group_permissions']:
             gname = g["group_name"]
